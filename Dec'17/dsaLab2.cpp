ae051d890ff00cf9372b7bb70d1a591f6cba1f31
tanujasawant <f2013658@goa.bits-pilani.ac.in>
2017-12-23 17:24:29 +0530

    Update dsaLab2.cpp

--- Dec'17/dsaLab2.cpp
+++ Dec'17/dsaLab2.cpp
@@ -21,3 +21,66 @@ int main() {
     cout<<fa(a,b);
     return 0;
 }
+
+//mergeSort
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> mergee(vector<int> arr1,vector<int> arr2){
+    vector <int> arr;
+    int n1=arr1.size();
+    int n2=arr2.size();
+    int i=0;
+    int j=0;
+    while(i+j<n1+n2){
+        if(i==n1){
+            arr.push_back(arr2.at(j++));
+        }
+        else if(j==n2){
+            arr.push_back(arr1.at(i++));
+        }
+        else if(arr1[i]<arr2[j])
+            arr.push_back(arr1[i++]);
+        else
+            arr.push_back(arr2[j++]);
+    }
+    return arr;
+}
+
+vector <int> sort(vector <int> arr){
+    if(arr.size()==1)
+        return arr;
+    int n1=arr.size()/2;
+    int n2=arr.size()-n1;
+    vector <int> arr1;
+    vector <int> arr2;
+    
+    for(int i=0; i<n1;i++)
+        arr1.push_back(arr[i]);
+    for(int i=0;i<n2;i++)
+        arr2.push_back(arr[n1+i]);
+    
+    
+     arr1 = sort(arr1);
+     arr2 = sort(arr2);
+    return mergee(arr1,arr2);
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int t;
+    vector <int> arr;
+    for(int i=0;i<n;i++){
+        cin>>t;
+        arr.push_back(t);
+    }
+    
+    arr=sort(arr);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
